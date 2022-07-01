97696dd8fc33baef3f9b36fbb2bb17418734318e
gulugulu1103 <gulugulu1103@qq.com>
2022-07-01 17:49:24 +0800

    将Pet与PetData解耦合

--- Models/Pet.cs
+++ Models/Pet.cs
@@ -6,12 +6,10 @@ namespace DesktopPet.Models
     {
         public string Name { get; set; } = "Unknown";
         public string Description { get; set; } = "No Description Here";
-        public double Health { get; set; } = 100;
         public double MaxHealth { get; set; } = 100;
-        public double Hunger { get; set; } = 100;
         public double MaxHunger { get; set; } = 100;
-        public double Happy { get; set; } = 100;
         public double MaxHappy { get; set; } = 100;
+        public double MinHappy { get; set; } = -100;
         public Dictionary<Status, string?> ImageSource { get; set; } = new Dictionary<Status, string?>();
     }
 }
